13a745e77a44982fc9c6206cf941c4351b925a13
Lexminimi <44340610+Lexminimi@users.noreply.github.com>
2024-08-27 16:09:06 +0200

    Setup 1
    Creating the app and models part 1

--- circe/settings.py
+++ circe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['fischerb2.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'hops.apps.HopsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hops/models.py
+++ hops/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Students(models.Model):
+    '''
+    Student names
+    '''
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(models.User)
+    classes = ArrayField(ArrayField(models.IntegerField()))
+
+class ClassGroups(models.Model):
+    '''
+    Group names - for example beginner class A,B - you can have multiple classes a week wich belongs to one group
+    '''
+    groupname = models.CharField(max_length=100)
+
+class Classes(models.Model):
+    '''
+    Class object
+    '''
+    teacher = models.ForeignKey(models.User)
+    name = models.CharField(max_length=100)
+    days = models.IntegerField()
+    hour = models.TimeField(auto_now=False, auto_now_add=False)
+    classgroup = models.ForeignKey(models.Classes)
