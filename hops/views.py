6861f61b19d46e543863c18ed0d975e39a966d1a
Lexminimi <44340610+Lexminimi@users.noreply.github.com>
2024-11-13 15:28:27 +0100

    gepcsere

--- hops/views.py
+++ hops/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 from .models import Students, ClassGroups, AttendanceSheet, Presence, AttendanceRecord
 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from .forms import AttendanceForm
-from django.contrib.auth.models import Group, User
+from django.http import HttpResponse, JsonResponse
 from rest_framework import permissions, viewsets, status
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from hops.serializers import GroupSerializer, UserSerializer, GroupsSerializer, AttendanceSerializer,StudentAttendance
 
 
@@ -36,7 +36,8 @@ import logging
 # Set up logging
 logger = logging.getLogger(__name__)
 
-@api_view(['POST', 'GET'])
+@api_view(['GET','POST'])
+@csrf_exempt
 def attendance( request, group_id, date=None):
     if request.method == 'GET':
         # Retrieve attendance record by group and date
@@ -45,30 +46,15 @@ def attendance( request, group_id, date=None):
         # Serialize attendance data
         serializer = AttendanceSerializer(attendance_sheet)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        logger.warning("Platform is running at risk POST")
-        today = datetime.today().date()
 
-        # Check if attendance for today and the group already exists
-        if Attendance.objects.filter(date__date=today, group_id=group_id).exists():
-            return Response({"message": "Attendance for this group already exists today."},
-                            status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = AttendanceSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
-        # Retrieve the group and its students
-        group = get_object_or_404(ClassGroups, id=group_id)
-        students_in_group = group.members.all()
-
-        # Create a new attendance record for each student in the group
-        attendance = Attendance.objects.create(date=today, group=group)
-        logger.info("Creation called")
-        attendance.students.set(students_in_group)  # Link all group students to the attendance record
-
-        # Set default presence for each student as needed
-        # (e.g., mark as "absent" initially or customize based on your logic)
-
-        # Serialize the created attendance data
-        serializer = AttendanceSerializer(attendance)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET'])
 def student_attendance( request, studentid):
